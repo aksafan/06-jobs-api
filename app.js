98e3bff926919a0dab33b7f4d836d431f9e939fc
aksafan <subzero_ck@ukr.net>
2025-01-26 01:57:55 -0600

    Add base frontend setup

--- app.js
+++ app.js
@@ -40,9 +40,7 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-});
+app.use(express.static('public'));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
