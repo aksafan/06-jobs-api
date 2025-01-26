ae3c4db17bcd0c0ba1fab7021c8b2720ff319cbe
aksafan <subzero_ck@ukr.net>
2025-01-26 01:08:07 -0600

    Add swagger

--- app.js
+++ app.js
@@ -7,10 +7,15 @@ const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 
-const connectDB = require('./db/connect');
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
 
 // auth
 const authenticateUser = require('./middleware/authentication');
@@ -36,8 +41,10 @@ app.use(cors());
 app.use(xss());
 
 app.get('/', (req, res) => {
-    res.send('jobs api');
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
